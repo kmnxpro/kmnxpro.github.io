de4fed96317dc4396b95107f05506a9919c390e6
kmnxpro <kmnxpro@gmail.com>
2018-01-24 15:18:38 -0500

    Append all video tracks to DIV

--- misc/samples/src/content/getusermedia/gum/js/main.js
+++ misc/samples/src/content/getusermedia/gum/js/main.js
@@ -27,7 +27,7 @@ function handleSuccess(stream) {
   // BEGIN Added 20180124
   var streamInfoDIV = document.getElementById('stream-info');
   //streamInfoDIV.innerText = videoTracks.length; //JSON.stringify(videoTracks);
-  streamInfoDIV.innerText = videoTracks.map(x => JSON.stringify(x));
+  streamInfoDIV.innerText = videoTracks.forEach(x => streamInfoDIV.innerText.append(x));
   ST = stream;
   VT = videoTracks;
   // END Added 20180124
