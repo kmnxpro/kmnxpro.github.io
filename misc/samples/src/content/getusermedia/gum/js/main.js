42089edf01a1da8e5fa6bbc9e42f85453ab1cd6f
kmnxpro <kmnxpro@gmail.com>
2018-01-24 15:21:14 -0500

    Concat all video track labels to DIV

--- misc/samples/src/content/getusermedia/gum/js/main.js
+++ misc/samples/src/content/getusermedia/gum/js/main.js
@@ -27,7 +27,7 @@ function handleSuccess(stream) {
   // BEGIN Added 20180124
   var streamInfoDIV = document.getElementById('stream-info');
   //streamInfoDIV.innerText = videoTracks.length; //JSON.stringify(videoTracks);
-  streamInfoDIV.innerText = videoTracks.forEach(x => streamInfoDIV.innerText.append(x));
+  streamInfoDIV.innerText = videoTracks.forEach(x => streamInfoDIV.innerText.concat(x.label));
   ST = stream;
   VT = videoTracks;
   // END Added 20180124
