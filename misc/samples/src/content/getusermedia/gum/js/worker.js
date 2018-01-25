6a2530556497726a51839e5ed69a2a3bf4029010
kmnxpro <kmnxpro@gmail.com>
2018-01-25 17:50:27 -0500

    Concat all video track labels to DIV -- v50

--- misc/samples/src/content/getusermedia/gum/js/worker.js
+++ misc/samples/src/content/getusermedia/gum/js/worker.js
@@ -5,8 +5,8 @@ onmessage = function(event) {
 
   /* Image Processing goes here */
   for (var i=0; i < dst.length; i += 4) {
-    dst[i + 0] = dst[i + 0] + 100;  // red
-    dst[i + 1] = dst[i + 1] - 70;  // green
+    dst[i + 0] = dst[i + 0] - 100;  // red
+    dst[i + 1] = dst[i + 1] + 70;  // green
     dst[i + 2] = dst[i + 2] - 80;  // blue
   }
 
